9f39058de1720cc538972e396bf6e572729d740c
Cynda3 <andergonzalez33@gmail.com>
2019-09-26 09:59:53 +0200

    Ejercicio 7 echo

---
+++ Ejercicio7.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>Ejericio 1</title>
+</head>
+<body>
+	<?php
+
+	$num = rand(2,50);
+
+	while ($num != 1) {
+		echo "$num </br>";
+		if ($num%2==0) {
+			$num = $num/2;
+		}else{
+			$num = $num*3+1;
+		}
+	}
+		echo "$num </br>";
+
+	?>
+
+</body>
+</html>
