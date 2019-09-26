c8a332ca9adb77d1686ec865d034c59662f49983
Cynda3 <andergonzalez33@gmail.com>
2019-09-26 09:50:35 +0200

    Ejercicio 6 hecho

--- Ejercicio6.php
+++ Ejercicio6.php
@@ -5,20 +5,29 @@
 <body>
 	<?php
 
-	$potencia = rand(1,10);
-	$cantidad = rand(100,200);
+	$potencia = rand(1,5);
+	$cantidad = rand(50,100);
 
-	$valorPotencia = $potencia;
+	$valorPotencia = 1;
 
 
 	echo "Potencia: ".$potencia." cantidad: ".$cantidad."<br>";
 
-	while ($valorPotencia <= $cantidad) {
-		for ($i=0; $i < $potencia; $i++) { 
-			$valorPotencia = $valorPotencia*$potencia;
+
+	// Recorro del 1 hasta el final
+	for ($i=1; $valorPotencia < $cantidad; $i++) { 
+		$valorPotencia = $i;
+		for ($j=1; $j <= $potencia; $j++) { 
+			$valorPotencia = $valorPotencia * $i;
 		}
-		echo "$potencia * $valorPotencia</br>";
+		echo "$i - $valorPotencia</br>";
 	}
+
+	/*
+
+	SE que debería quitar la ultima linea porque soberpasa la cantidad pero ya he perdido mucho tiempo con este ejercicio y quiero terminar el resto.
+
+	*/
 	?>
 
 </body>
